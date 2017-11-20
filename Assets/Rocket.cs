dbaa236f0498f347da940c7560024526af1a7376
Nik Ambukovski <lazokin@hotmail.com>
2017-11-20 19:07:37 +1100

    Resolving Movement Bugs

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -8,18 +8,17 @@ public class Rocket : MonoBehaviour {
     Rigidbody rigidBody;
     AudioSource audioSource;
 
-	// Use this for initialization
 	void Start () {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-        ProcessInput();
+        Thrust();
+        Rotate();
 	}
 
-    private void ProcessInput() {
+    private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
             if (!audioSource.isPlaying) {
@@ -28,11 +27,15 @@ public class Rocket : MonoBehaviour {
         } else {
             audioSource.Stop();
         }
+    }
 
+    private void Rotate() {
+        rigidBody.freezeRotation = true;
         if (Input.GetKey(KeyCode.A)) {
             transform.Rotate(Vector3.forward);
         } else if (Input.GetKey(KeyCode.D)) {
             transform.Rotate(Vector3.back);
         }
+        rigidBody.freezeRotation = false;
     }
 }
